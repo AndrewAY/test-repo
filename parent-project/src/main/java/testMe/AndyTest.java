8fb35207926ff5dc13eca63ea536ad951db9b362
AAu-Young <AAu-Young@DELLXPS-24.airas.lan>
2020-07-22 11:12:04 +0100

    2nd commit

--- parent-project/src/main/java/testMe/AndyTest.java
+++ parent-project/src/main/java/testMe/AndyTest.java
@@ -4,7 +4,7 @@ public class AndyTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("dude!");
+			System.out.println("dude, where's my car?!");
 	}
 
 }
